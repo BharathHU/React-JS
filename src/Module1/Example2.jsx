930af7e7fc2ae7b5785117b0ea1e56053e40a2fb
BharathHU <bharathhubharath@gmail.com>
2026-05-28 10:34:49 +0530

    updated

---
+++ src/Module1/Example2.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Example2() {
+  return (
+    <div>
+      <h1>Employee Details</h1>
+      <p>Name:Bharath</p>
+      <p>Role:Software Engineer</p>
+      <p>CurrentDate:{new Date().toLocaleDateString()}</p>
+      
+    </div>
+
+  )
+}
+
+export default Example2
