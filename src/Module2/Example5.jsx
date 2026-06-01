5219c95080bf8e7ca1ee64b81b48d7931aca6353
BharathHU <bharathhubharath@gmail.com>
2026-06-01 14:47:09 +0530

    updated

--- src/Module2/Example5.jsx
+++ src/Module2/Example5.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import cars from '../assets/hero.png'
+import cars from '../assets/bmw.jpg'
 function Example5() {
     let direction="rtl"
     let direction2="ltr"
@@ -7,16 +7,16 @@ function Example5() {
 let content="csk"
 let height="200px"
 let width="200px"
- let imageURL="https://www.cricbuzz.com/a/img/v1/192x192/i1/c170508/chennai-super-kings.jpg"
+ let imageURL="https://imgs.search.brave.com/9TN8Q4tyAiTii8Mej-GdHEYz5P1Wm8Tw-qT9GbFW-I8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMDcv/NDQ5LzE4NC9zbWFs/bC90cmVlLWdyb3d0/aC1vbi1nbG9iZS1n/bGFzcy1pbi1uYXR1/cmUtY29uY2VwdC1l/Y28tZWFydGgtZGF5/LWZyZWUtcGhvdG8u/anBn"
  let images=[
-    "https://www.cricbuzz.com/a/img/v1/192x192/i1/c170508/chennai-super-kings.jpg",
-    "https://www.cricbuzz.com/a/img/v1/192x192/i1/c170508/chennai-super-kings.jpg",]
+    "https://imgs.search.brave.com/mV3qSLo3U01PA6zivTpBGI6xGvZpsFcfDRrcRZgfO2I/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjgv/ODI4LzM4My9zbWFs/bC9hLWdsYXNzLWJh/bGwtd2l0aC1sYW5k/c2NhcGVzLWluc2lk/ZS1vZi1pdC1uYXR1/cmUtc3RvY2stcGhv/dG8uanBn",
+    "https://imgs.search.brave.com/hkBJtkFd8gmIj3_fJYElvKIdzmyj0aP23bgacz-r6PI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQ0/Mzg2NDY3OS9waG90/by9mZWVsLXRoZS1i/ZWF1dHktb2YtbmF0/dXJlLXdpdGgtYWxs/LXlvdXItc2Vuc2Vz/LmpwZz9zPTYxMng2/MTImdz0wJms9MjAm/Yz1wSEFaU0tod0FO/aVZDcXNDME9vT3B1/OVNMZ3FoS2lIWUdB/eG5aOExkd3FFPQ",]
     return (
     <div>
       <bdo dir={direction2}>Hello</bdo>
       <abbr title={content}>{title}</abbr>
       {/* 1st way to add image */}
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh8n2sXo7l3mL1a5j6qj8v9e7u2z5kKZg&usqp=CAU" alt="no image" height={height} width={width}/>
+      <img src="https://imgs.search.brave.com/Jlb773O-QMFQDtUsVoFhrQnCpy-WDP5-e1c3ejIWhyE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzUv/MjU3LzMxOC9zbWFs/bC9haS1nZW5lcmF0/ZWQtZWFydGgtbmF0/dXJlLWVhcnRoLWlu/LWEtYmFsbC1uYXR1/cmUtaW4tYS1iYWxs/LWdlbmVyYXRpdmUt/YWktcGhvdG8uanBn" alt="no image" height={height} width={width}/>
       {/* 2nd way to add image */}
       <img src={imageURL} alt="no image" height={height} width={width}/>
         {/* 3rd way to add image */}
